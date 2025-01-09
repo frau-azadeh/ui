6a3a63cc4e0c568372764e7574b24f81f822a415
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-01-09 05:50:25 -0800

    add all

---
+++ webapp/index.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/m/Text"
+], 
+    function(Text){
+        "use strict";
+        new Text({
+            text:"hi azadehhhhhh"
+        }).placeAt("content");
+        alert("hiiiii");
+})
